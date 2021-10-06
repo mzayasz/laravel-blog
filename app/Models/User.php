57fed56c70ce38a7f39c02dd3bb5847b0ad7f913
MartinZayasZ <zayasmartin70@gmail.com>
2021-10-05 23:13:29 -0700

    se instalo jetstream

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,14 +2,21 @@
 
 namespace App\Models;
 
+use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Fortify\TwoFactorAuthenticatable;
+use Laravel\Jetstream\HasProfilePhoto;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens;
+    use HasFactory;
+    use HasProfilePhoto;
+    use Notifiable;
+    use TwoFactorAuthenticatable;
 
     /**
      * The attributes that are mass assignable.
@@ -30,6 +37,8 @@ class User extends Authenticatable
     protected $hidden = [
         'password',
         'remember_token',
+        'two_factor_recovery_codes',
+        'two_factor_secret',
     ];
 
     /**
@@ -41,15 +50,12 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    //relación uno a uno
-    public function profile()
-    {
-        return $this->hasOne(Profile::class);
-    }
-
-    //relación uno a muchos
-    public function posts()
-    {
-        return $this->hasMany(Post::class);
-    }
+    /**
+     * The accessors to append to the model's array form.
+     *
+     * @var array
+     */
+    protected $appends = [
+        'profile_photo_url',
+    ];
 }
